8d89e3717c099ffb7c09a4510a9ffbaa45d14068
Natela <n.kerd@mail.ru>
2021-12-18 21:31:28 +0300

    module5-task1

--- src/presenter/movie-presenter.js
+++ src/presenter/movie-presenter.js
@@ -22,43 +22,89 @@ constructor(moviesContainer, movie, movieListPresenter) {
 
 init = () => {
   const cardComponent = new CardsView(this.#movie);
+  this.#popupComponent = new PopupView(this.#movie);
   renderElement(this.#moviesContainer, cardComponent, RenderPosition.BEFOREEND);
-  this.addToFavorite(this.#movieListPresenter, cardComponent);
-  this.addToWatchlist(this.#movieListPresenter, cardComponent);
-  this.addToHistory(this.#movieListPresenter, cardComponent);
+  this.addToFavorite(cardComponent);
+  this.addToWatchlist(cardComponent);
+  this.addToHistory(cardComponent);
   this.addPostClickHandler(cardComponent, () => {
-    this.#popupComponent = new PopupView(this.#movie);
+
     this.#popupComponent.postClickHandler(this.#movie, cardComponent);
+    this.addToFavoriteInPopup(cardComponent);
+    this.addToWatchlistInPopup(cardComponent);
+    this.addToHistoryInPopup(cardComponent);
   });
 }
 
-addToFavorite(presenter, cardComponent) {
+addToFavoriteInPopup(cardComponent) {
+  const favoriteButton = this.#popupComponent.element.querySelector('.film-details__control-button--favorite');
+  const favoriteButtonInCard = cardComponent.element.querySelector('.film-card__controls-item--favorite');
+  favoriteButton.addEventListener('click', () => {
+    const favoriteSign = cardComponent.movieInfo.userDetails.favorite;
+    cardComponent.movieInfo.userDetails.favorite = !favoriteSign;
+    favoriteButton.classList.toggle('film-details__control-button--active');
+    favoriteButtonInCard.classList.toggle('film-card__controls-item--active');
+    this.#movieListPresenter.menuComponent.setFiltersCount();
+  });
+}
+
+addToWatchlistInPopup(cardComponent) {
+  const watchlistButton = this.#popupComponent.element.querySelector('.film-details__control-button--watchlist');
+  const watchlistButtonInCard = cardComponent.element.querySelector('.film-card__controls-item--add-to-watchlist');
+  watchlistButton.addEventListener('click', () => {
+    const watchlistSign = cardComponent.movieInfo.userDetails.watchlist;
+    cardComponent.movieInfo.userDetails.watchlist = !watchlistSign;
+    watchlistButton.classList.toggle('film-details__control-button--active');
+    watchlistButtonInCard.classList.toggle('film-card__controls-item--active');
+    this.#movieListPresenter.menuComponent.setFiltersCount();
+  });
+}
+
+addToHistoryInPopup(cardComponent) {
+  const alreadyWatchedButton = this.#popupComponent.element.querySelector('.film-details__control-button--watched');
+  const alreadyWatchedButtonInCard = cardComponent.element.querySelector('.film-card__controls-item--mark-as-watched');
+  alreadyWatchedButton.addEventListener('click', () => {
+    const alreadyWatchedSign = cardComponent.movieInfo.userDetails.alreadyWatched;
+    cardComponent.movieInfo.userDetails.alreadyWatched = !alreadyWatchedSign;
+    alreadyWatchedButton.classList.toggle('film-details__control-button--active');
+    alreadyWatchedButtonInCard.classList.toggle('film-card__controls-item--active');
+    this.#movieListPresenter.menuComponent.setFiltersCount();
+  });
+}
+
+addToFavorite(cardComponent) {
   const favoriteButton = cardComponent.element.querySelector('.film-card__controls-item--favorite');
+  const favoriteButtonInPopup = this.#popupComponent.element.querySelector('.film-details__control-button--favorite');
   favoriteButton.addEventListener('click', () => {
     const favoriteSign = cardComponent.movieInfo.userDetails.favorite;
     cardComponent.movieInfo.userDetails.favorite = !favoriteSign;
     favoriteButton.classList.toggle('film-card__controls-item--active');
-    presenter.menuComponent.setFiltersCount();
+    favoriteButtonInPopup.classList.toggle('film-details__control-button--active');
+    this.#movieListPresenter.menuComponent.setFiltersCount();
   });
 }
 
-addToWatchlist(presenter, cardComponent) {
+addToWatchlist(cardComponent) {
   const watchlistButton = cardComponent.element.querySelector('.film-card__controls-item--add-to-watchlist');
+  const watchlistButtonInPopup = this.#popupComponent.element.querySelector('.film-details__control-button--watchlist');
   watchlistButton.addEventListener('click', () => {
     const watchlistSign = cardComponent.movieInfo.userDetails.watchlist;
     cardComponent.movieInfo.userDetails.watchlist = !watchlistSign;
     watchlistButton.classList.toggle('film-card__controls-item--active');
-    presenter.menuComponent.setFiltersCount();
+    watchlistButtonInPopup.classList.toggle('film-details__control-button--active');
+    this.#movieListPresenter.menuComponent.setFiltersCount();
   });
 }
 
-addToHistory(presenter, cardComponent) {
+addToHistory(cardComponent) {
   const alreadyWatchedButton = cardComponent.element.querySelector('.film-card__controls-item--mark-as-watched');
+  const alreadyWatchedButtonInPopup = this.#popupComponent.element.querySelector('.film-details__control-button--watched');
   alreadyWatchedButton.addEventListener('click', () => {
     const alreadyWatchedSign = cardComponent.movieInfo.userDetails.alreadyWatched;
     cardComponent.movieInfo.userDetails.alreadyWatched = !alreadyWatchedSign;
     alreadyWatchedButton.classList.toggle('film-card__controls-item--active');
-    presenter.menuComponent.setFiltersCount();
+    alreadyWatchedButtonInPopup.classList.toggle('film-details__control-button--active');
+    this.#movieListPresenter.menuComponent.setFiltersCount();
   });
 }
 
